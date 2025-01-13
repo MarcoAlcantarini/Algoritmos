a18d647ad824da4d690d77f787e1aa112ad462ea
MarcoAlcantarini <marco.alcantarini@correo.unimet.edu.ve>
2025-01-13 09:43:07 -0400

    Add files via upload

---
+++ tarea2.py
@@ -0,0 +1,21 @@
+print ("Ingrese los ingredientes")
+print ("Tazas de Agua")
+var1 = int(input (" "))
+print ("Tazas de Harina")
+var2 = int(input (" "))
+print ("Cucharaditas de Sal")
+var3 = int(input (" "))
+print ("Cucharadas de Aceite")
+var4 = int(input (" ") )
+var1 = var1 * 48
+print (var1)
+var2 = var2 * 48
+print (var2)
+var4 =  var4 * 3
+print (var4)
+conjunto1 = float (var1 + var2 + var3 + var4)
+print (conjunto1)
+conjunto1 = conjunto1 - (conjunto1 * 0.2)
+
+
+print ("El volumen de tu arepa es de", conjunto1) 
