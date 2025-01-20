580ba6fa7df1dfb517c027f500249946b3180301
MarcoAlcantarini <marco.alcantarini@correo.unimet.edu.ve>
2025-01-19 22:35:37 -0400

    Update tarea4.py

--- tarea4.py
+++ tarea4.py
@@ -1,14 +1,14 @@
 fecha = int(input("Coloque un año que se encuentre entre 1900 y 2199: "))
 contador = int()
-comienzo = int(1899)
+comienzo = int(1900)
 
-while comienzo <= fecha:
+
+for comienzo in range(comienzo, fecha +1):
     if (comienzo % 4 == 0 and (comienzo % 100 != 0 or comienzo % 400 == 0)): 
         contador = contador + 1
-        comienzo = comienzo + 1
     
     else:
-        comienzo = comienzo + 1
+        contador = contador
     
 if 1900 < fecha < 2199:
     print ("El numero de años bisiestos entre 1900 y ", fecha,  "es: ", contador)
